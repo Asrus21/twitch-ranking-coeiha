1bbcf3ed5963614c5ec086ad6af7f162a005127c
Thiago Arruda <tarruda1513@gmail.com>
2026-05-27 23:11:12 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: 'https://twitch-ranking-coeiha.vercel.app',
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'static-cdn.jtvnw.net' },
       { protocol: 'https', hostname: 'decapi.me' },
+      { protocol: 'https', hostname: 'api.dicebear.com' },
     ],
   },
 };
